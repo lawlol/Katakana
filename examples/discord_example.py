9187977275eb2ac7df87ca26fc0997adcdaf44f0
law <161666444+lawlol@users.noreply.github.com>
2025-01-07 00:07:37 -0500

    Update discord_example.py

--- examples/discord_example.py
+++ examples/discord_example.py
@@ -3,4 +3,4 @@ from katakana.core.loader import LookupRunner, LookupType
 token = ""  # Bot token
 runner = LookupRunner(discord_token=token)
 
-print(runner.run_lookup(LookupType.DISCORD_USER, 1219031488016679033))
+print(runner.run_lookup(LookupType.DISCORD_USER, 1219031488016679033)) # User ID here, that is my user ID as an example.
